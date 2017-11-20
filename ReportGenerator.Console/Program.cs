f39d55bc98b9b020f4aca09377b2799490bc249c
Sijibomi Ajayi <sijiajayi1@gmail.com>
2017-11-20 01:01:18 +0100

    Implemented Generate(), what is left now is formatting. Tables are not easy to draw in console apps but I'm sure we can make it work with UI.

--- ReportGenerator.Console/Program.cs
+++ ReportGenerator.Console/Program.cs
@@ -13,25 +13,40 @@ namespace ReportGenerator.Console
 
             var reportGenerator = new SampleModelReportGenerator();
 
+
             var reportTitle = reportGenerator.Title;
             var reportHeaders = reportGenerator.Headers;
 
+            var sampleModelObject1 = new SampleModel(15, "Tola", "Lagos", "female");
+            var sampleModelObject2 = new SampleModel(23, "Deji", "Lagos", "male");
+            var sampleModelObject3 = new SampleModel(32, "Uzo", "Lagos", "male");
+            var sampleModelObject4 = new SampleModel(44, "Bala", "Lagos", "female");
+
+            List<SampleModel> sampleList = new List<SampleModel>();
+            sampleList.Add(sampleModelObject1);
+            sampleList.Add(sampleModelObject2);
+            sampleList.Add(sampleModelObject3);
+            sampleList.Add(sampleModelObject4);
+
+            
             System.Console.WriteLine($"##################{reportTitle}#################");
             System.Console.WriteLine();
             System.Console.WriteLine();
-            // reportHeaders.ForEach(x => { System.Console.Write("{0,-20}",x); }); //formats the string with spacing and alignments
-            try
-            {
+            
+            //try
+            //{
                 foreach (var header in reportHeaders)
                 {
                     System.Console.Write("{0,-20}", header.Value); //formats the string with spacing and alignments
                 }
+                System.Console.WriteLine();
+                reportGenerator.Generate(sampleList);
                 System.Console.ReadLine();
-            }
-            catch
-            {
-                System.Console.WriteLine("Error occured!");  //vague error
-            }
+            //}
+            //catch
+            //{
+            //    System.Console.WriteLine("Error occured!");  //vague error
+            //}
         }
     }
 }
